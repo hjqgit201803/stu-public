cc7d52ea554bbf1813ba9c034a1ce3e9bea2e6ed
hejianqing <17666143899@163.com>
2019-12-06 23:13:26 +0800

    首次提交

---
+++ src/main/java/com/hjq/stupublic/StuPublicApplication.java
@@ -0,0 +1,13 @@
+package com.hjq.stupublic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StuPublicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StuPublicApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/hjq/stupublic/controller/testController.java
@@ -0,0 +1,14 @@
+package com.hjq.stupublic.controller;
+
+/**
+ * @author hejianqing
+ * @date 2019/12/6
+ */
+
+public class testController {
+
+    public void testGitCommit(){
+        System.out.println("this is nothing hhaha");
+    }
+
+}
