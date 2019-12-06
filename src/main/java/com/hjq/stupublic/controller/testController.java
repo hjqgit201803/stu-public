12be2947773aae8c9206e9259fe3f84563b20ba6
hejianqing <17666143899@163.com>
2019-12-06 23:25:44 +0800

    pom 修改

--- src/main/java/com/hjq/stupublic/controller/testController.java
+++ src/main/java/com/hjq/stupublic/controller/testController.java
@@ -1,10 +1,14 @@
 package com.hjq.stupublic.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author hejianqing
  * @date 2019/12/6
  */
-
+@RestController
+@RequestMapping(value = "/test")
 public class testController {
 
     public void testGitCommit(){
